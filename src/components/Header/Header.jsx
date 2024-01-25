1e84eabc8240c854deff207904ae994d971787d7
KramYengir <kramyengir@gmail.com>
2024-01-25 19:07:02 +0000

    tweaked header size & layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,29 +8,33 @@ const Header = () => {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <header className="container">
-      <nav>
-        <img src={logo} alt="Site Logo" />
-        <button onClick={() => setIsActive(!isActive)}>
-          <img src={hamburger} alt="Hamburger menu button" />
-        </button>
-        <ul className={isActive ? "active" : ""}>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <h1>We Are Creatives!</h1>
-      <img className="arrow-down" src={arrowDownImg} alt="" />
+    <header>
+      <div className="container">
+        <nav>
+          <img src={logo} alt="Site Logo" />
+          <button onClick={() => setIsActive(!isActive)}>
+            <img src={hamburger} alt="Hamburger menu button" />
+          </button>
+          <ul className={isActive ? "active" : ""}>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <h1>
+          We Are <br /> Creatives!
+        </h1>
+        <img className="arrow-down" src={arrowDownImg} alt="" />
+      </div>
     </header>
   );
 };
