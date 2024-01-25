72c07d6fcc0e8110ba0126c28f1ff7ac8ef61416
KramYengir <kramyengir@gmail.com>
2024-01-25 13:13:51 +0000

    add logo, button & nav to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,35 @@
 import "./Header.css";
+import logo from "../../assets/images/logo.svg";
+import hamburger from "../../assets/images/icon-hamburger.svg";
+import { useState } from "react";
 
 const Header = () => {
-  return <header>Header</header>;
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <header className="container">
+      <nav>
+        <img src={logo} alt="Site Logo" />
+        <button onClick={() => setIsActive(!isActive)}>
+          <img src={hamburger} alt="Hamburger menu button" />
+        </button>
+        <ul className={isActive ? "active" : ""}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
