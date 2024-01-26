fcd9d298e07c6f2ca4e868c22115ab4817ee1934
KramYengir <kramyengir@gmail.com>
2024-01-26 13:32:06 +0000

    can determine & style active links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,34 +6,69 @@ import { useState } from "react";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
+  const [activeLink, setActiveLink] = useState();
+
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+    console.log("Active libk: ", linkName);
+    setIsActive(false);
+  };
 
   return (
     <header>
       <div className="container">
-        <nav>
+        <nav role="navigation" aria-label="Main menu" aria-expanded={isActive}>
           <img src={logo} alt="Site Logo" />
           <button onClick={() => setIsActive(!isActive)}>
             <img src={hamburger} alt="Hamburger menu button" />
           </button>
           <ul className={isActive ? "active" : ""}>
             <li>
-              <a href="#about">About</a>
+              <a
+                href="#about"
+                className={activeLink == "#about" ? "active" : ""}
+                onClick={() => handleLinkClick("#about")}
+              >
+                About
+              </a>
             </li>
             <li>
-              <a href="#services">Services</a>
+              <a
+                href="#services"
+                className={activeLink == "#services" ? "active" : ""}
+                onClick={() => handleLinkClick("#services")}
+              >
+                Services
+              </a>
             </li>
             <li>
-              <a href="#projects">Projects</a>
+              <a
+                href="#projects"
+                className={activeLink == "#projects" ? "active" : ""}
+                onClick={() => handleLinkClick("#projects")}
+              >
+                Projects
+              </a>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <a
+                href="#contact"
+                className={activeLink == "#contact" ? "active" : ""}
+                onClick={() => handleLinkClick("#contacts")}
+              >
+                Contact
+              </a>
             </li>
           </ul>
         </nav>
         <h1>
           We Are <br /> Creatives!
         </h1>
-        <img className="arrow-down" src={arrowDownImg} alt="" />
+        <img
+          className="arrow-down"
+          src={arrowDownImg}
+          alt="Arrow pointing down"
+        />
       </div>
     </header>
   );
