76640e6818cf2368aaeb5b597201a9f3971112ce
KramYengir <kramyengir@gmail.com>
2024-02-07 13:13:50 +0000

    complete gallery desktop layout

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,6 +3,10 @@ import bottlesIMG from "../../assets/images/mobile/image-gallery-milkbottles.jpg
 import coneIMG from "../../assets/images/mobile/image-gallery-cone.jpg";
 import orangeIMG from "../../assets/images/mobile/image-gallery-orange.jpg";
 import sugarCubesIMG from "../../assets/images/mobile/image-gallery-sugar-cubes.jpg";
+import bottlesDesktopIMG from "../../assets/images/desktop/image-gallery-milkbottles.jpg";
+import coneDesktopIMG from "../../assets/images/desktop/image-gallery-cone.jpg";
+import orangeDesktopIMG from "../../assets/images/desktop/image-gallery-orange.jpg";
+import sugarCubesDesktopIMG from "../../assets/images/desktop/image-gallery-sugarcubes.jpg";
 
 const Gallery = () => {
   return (
@@ -11,6 +15,26 @@ const Gallery = () => {
       <img src={orangeIMG} alt="Image of sliced orange on a plate" />
       <img src={coneIMG} alt="Image of an empty ice-cream cone" />
       <img src={sugarCubesIMG} alt="Image of stacked sugar cubes" />
+      <img
+        className="desktop"
+        src={bottlesDesktopIMG}
+        alt="Image of milk bottles"
+      />
+      <img
+        className="desktop"
+        src={orangeDesktopIMG}
+        alt="Image of sliced orange on a plate"
+      />
+      <img
+        className="desktop"
+        src={coneDesktopIMG}
+        alt="Image of an empty ice-cream cone"
+      />
+      <img
+        className="desktop"
+        src={sugarCubesDesktopIMG}
+        alt="Image of stacked sugar cubes"
+      />
     </section>
   );
 };
